62132f60577a3c3f1f0d91f1d13062e1b951b318
Austin Jess <ajess33@gmail.com>
2020-02-11 21:00:13 -0600

    setup spotify authentication

---
+++ src/utils.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const getToken = () => {
+  const hash = window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial, item) => {
+      if (item) {
+        var parts = item.split('=');
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+      }
+      return initial;
+    }, {});
+  window.location.hash = '';
+
+  // set token
+  const _token = hash.access_token;
+  return _token;
+};
+
+export const spotifyAuthCheck = async () => {
+  const _token = getToken();
+  console.log(process.env.REACT_APP_REDIRECT_URI);
+  const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+
+  if (!_token) window.location = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=token`;
+
+  const config = {
+    method: 'GET',
+    url: AUTH_ENDPOINT,
+    headers: { Authorization: `Bearer ${_token}` }
+  }
+
+  let response = await axios(config);
+  console.log(response);
+}
